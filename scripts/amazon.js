399fd407544b9769e0cbad6e243c9e18aa053b76
AdarshMohanS <adarshwaitzz@gmail.com>
2024-09-30 06:31:26 +0530

    Move addToCart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML='';
@@ -57,50 +57,16 @@ products.forEach((product) => {
     `;
 
 });
+
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
 
-// We're going to use an object to save the timeout ids.
-// The reason we use an object is because each product
-// will have its own timeoutId. So an object lets us
-// save multiple timeout ids for different products.
-// For example:
-// {
-//   'product-id1': 2,
-//   'product-id2': 5,
-//   ...
-// }
-// (2 and 5 are ids that are returned when we call setTimeout).
-const addedMessageTimeouts = {};
-
-document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
-  button.addEventListener('click',() => {
-  const {productId}=button.dataset;
-
-  let matchingItem;
-
-  cart.forEach((item) => {
-    if(productId=== item.productId){
-      matchingItem = item;
-    }
-  });
-
-  const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-  const quantity= Number(quantitySelector.value);
-
-  if(matchingItem){
-    matchingItem.quantity+=quantity;
-
-  } else {
-    cart.push({
-      productId,
-      quantity
-    });
-  }
+function updateCartQuantity(productId){
   let cartQuantity = 0;
 
-  cart.forEach((item) => {
-    cartQuantity+=item.quantity;
+  cart.forEach((cartItem) => {
+    cartQuantity+=cartItem.quantity;
   });
+
   document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 
   const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
@@ -119,6 +85,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
   // Save the timeoutId for this product
   // so we can stop it later if we need to.
   addedMessageTimeouts[productId] = timeoutId;
+}
+// We're going to use an object to save the timeout ids.The reason we use an object is because each product will have its own timeoutId. So an object lets us save multiple timeout ids for different products
+const addedMessageTimeouts = {};
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
+  button.addEventListener('click',() => {
+  const {productId}=button.dataset;
+  addToCart(productId);
+  updateCartQuantity(productId);
 
   });
 });
